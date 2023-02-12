44d970f25126f13e856ad2d2fd7d006c232a6c5d
Maryana <mariana123ru@gmail.com>
2023-02-12 18:56:21 +0300

    Working version

--- src/quickstart.py
+++ src/quickstart.py
@@ -41,7 +41,7 @@ def main():
         # Call the Calendar API
         now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
         print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='Red', timeMin=now,
+        events_result = service.events().list(calendarId='primary', timeMin=now,
                                               maxResults=10, singleEvents=True,
                                               orderBy='startTime').execute()
         events = events_result.get('items', [])
